d66bfd8f65a72f92da6e7f499c7daaf0d7e089dc
fenix <a.sydorenko87@gmail.com>
2018-03-18 15:34:50 +0200

    #refactor (index.js, package-lock.json, package.json) add HttpError export, version update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
 const Application = require('./models/application')
+const HttpError = require('./models/httpError')
 
 module.exports = Application
+module.exports.Application = Application
+module.exports.default = Application
+module.exports.HttpError = HttpError
